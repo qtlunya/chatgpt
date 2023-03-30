7aa2530f4d62738c65f51a73b049434bd54eaa6b
alexia <me@alexia.lol>
2023-03-30 23:30:43 +0200

    fix(cli): make function async to fix aiohttp error

--- chatgpt/cli.py
+++ chatgpt/cli.py
@@ -1,5 +1,6 @@
 import asyncio
 import readline  # noqa: F401
+from functools import wraps
 
 import cloup
 from cloup.constraints import mutually_exclusive
@@ -7,15 +8,24 @@ from cloup.constraints import mutually_exclusive
 from . import ChatGPTClient
 
 
+def coroutine(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        return asyncio.run(f(*args, **kwargs))
+
+    return wrapper
+
+
 @cloup.command()
 @cloup.option("-i", "--initial-prompt", type=str, help="The initial prompt to use for ChatGPT.")
 @cloup.option("-n", "--no-initial-prompt",  is_flag=True, help="Suppress the default initial prompt for ChatGPT.")
 @cloup.constraint(mutually_exclusive, ["initial_prompt", "no_initial_prompt"])
-def cli(initial_prompt: str | None, no_initial_prompt: bool) -> None:
+@coroutine
+async def cli(initial_prompt: str | None, no_initial_prompt: bool) -> None:
     client = ChatGPTClient(initial_prompt=False if no_initial_prompt else initial_prompt)
     while True:
         try:
             prompt = input("> ")
-            print(asyncio.run(client.get_completion(prompt)))
+            print(await client.get_completion(prompt))
         except (EOFError, KeyboardInterrupt):
             break
